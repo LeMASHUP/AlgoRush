678bf4311e5c9fbcc1fd02a7cb099478c9f6f3ed
Zormz <115802948+Zormzy@users.noreply.github.com>
2023-01-31 15:28:06 +0100

    Implementation of SFPhysics

--- AlgoRush/main.cpp
+++ AlgoRush/main.cpp
@@ -1,7 +1,19 @@
-#include  "SFML/Graphics.hpp" // replace quotes with less than and greater than symbols
+#include "SFML/Graphics.hpp"
+#include "SFPhysics.h"
+
+#include <iostream>
+#include <string>
 
 int main()
 {
+    srand(time(0));
+
+    Vector2f gravity(0, 1.0);
+    sfp::World world(gravity);
+
+    sf::Clock clock;
+    sf::Time lastTime = clock.getElapsedTime();
+
     sf::RenderWindow window(sf::VideoMode(800, 600), "Not Tetris");
 
     sf::Event event;
@@ -14,6 +26,17 @@ int main()
 
                 window.close();
             }
+        }
+
+        sf::Time currentTime = clock.getElapsedTime();
+        unsigned long elapsedMs = (currentTime - lastTime).asMilliseconds();
+
+        if (elapsedMs > 0) {
+            lastTime = currentTime;
+            world.UpdatePhysics(elapsedMs);
+            window.clear(sf::Color::Black);
+            //window.draw(player);
+            window.display();
         }
     }
 
