e266330e3711bfc3fd7d141440bf7c044d624445
DDoryan <D.DECROIX@lahorde.school>
2023-02-03 15:08:54 +0100

    object

---
+++ AlgoRush/Object.cpp
@@ -0,0 +1,20 @@
+#include "SFML/Graphics.hpp"
+#include "SFPhysics.h"
+#include <iostream>
+#include "Object.h"
+
+Object::Object(sf::Texture texture, Vector2f scale) : m_texture(texture), m_scale(scale), m_draw(true)
+{
+	this->setScale(m_scale);
+	this->setTexture(m_texture);
+}
+
+void Object::throwObject()
+{
+	this->setVelocity(Vector2f(10, 20));
+}
+
+bool Object::iscollide(Object object)
+{
+	return this->collideWith(object).hasCollided;
+}
---
+++ AlgoRush/Object.h
@@ -0,0 +1,17 @@
+#include "SFML/Graphics.hpp"
+#include "SFPhysics.h"
+#include <iostream>
+
+class Object : public sfp::PhysicsSprite {
+private:
+
+	sf::Texture m_texture;
+	Vector2f m_scale;
+	bool m_draw;
+
+public:
+
+	Object(sf::Texture texture, Vector2f scale);
+	void throwObject();
+	bool iscollide(Object object);
+};
