bb9ac7b9a41058c5c9d2d286352cd64d64111a38
LeMASHUP <dutertre.zakaria@gmail.com>
2023-02-06 14:45:06 +0100

    Update main.cpp

--- AlgoRush/main.cpp
+++ AlgoRush/main.cpp
@@ -3,13 +3,23 @@
 
 #include <iostream>
 #include <string>
+#include <windows.h>
+#include <cstdio>
 
 #include "Character.h"
 #include "Level1.h"
 #include "Level2.h"
 
+#pragma execution_character_set( "utf-8" )
+
 int main()
 {
+    SetConsoleOutputCP(65001);
+
+    int showsquare = 0;
+    int posx = 25;
+    int posy = 200;
+
     int state = 1;
     bool levelCreated = false;
 
@@ -25,6 +35,17 @@ int main()
     window.setFramerateLimit(60);
 
     sf::Event event;
+
+    //Création d'un carré
+    sf::RectangleShape square(sf::Vector2f(100, 100));
+    square.setFillColor(sf::Color::Green);
+    square.setPosition(50, 50);
+
+    sf::RectangleShape square3(sf::Vector2f(100, 100));
+    square3.setFillColor(sf::Color::Red);
+    square3.setPosition(200, 50);
+
+    std::vector<sf::RectangleShape> squares;
 
     //Creation of levels and character
     Level1 level1(&world);
@@ -67,6 +88,32 @@ int main()
 
                 window.close();
             }
+
+            if (event.type == sf::Event::MouseButtonPressed && square.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+            {
+                sf::RectangleShape square2(sf::Vector2f(50, 50));
+                square2.setFillColor(sf::Color::Green);
+                square2.setPosition(posx, posy);
+                squares.push_back(square2);
+                posx += 60;
+                std::cout << "Hello World!" << std::endl;
+            }
+
+            if (event.type == sf::Event::MouseButtonPressed && square3.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+            {
+                sf::RectangleShape square4(sf::Vector2f(50, 50));
+                square4.setFillColor(sf::Color::Red);
+                square4.setPosition(posx, posy);
+                squares.push_back(square4);
+                posx += 60;
+                std::cout << "Hello World2!" << std::endl;
+            }
+
+            if (posx > 1575) {
+                posx = 25;
+                posy += 60;
+            }
+
             if (event.key.code == sf::Keyboard::D) character.forward();
             if (event.key.code == sf::Keyboard::Q) character.backward();
             if (event.key.code == sf::Keyboard::Z) character.jumpForward();
@@ -91,6 +138,10 @@ int main()
                 window.draw(character);
             }
             world.VisualizeAllBounds(window);
+            window.draw(square);
+            window.draw(square3);
+            for (const auto& s : squares)
+                window.draw(s);
             window.display();
         }
     }
