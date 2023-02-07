e9ccf6867f728f86009cd2c3ac209c1c6e157779
LeMASHUP <dutertre.zakaria@gmail.com>
2023-02-07 14:45:36 +0100

    Added Delete Button
    Correction du block manager et implémentation du bouton de suppression d'un bloc généré

--- AlgoRush/BlockManager.cpp
+++ AlgoRush/BlockManager.cpp
@@ -7,7 +7,7 @@
 
 BlockManager::BlockManager(sfp::World* world)
 {
-	for (int i = 0; i < 7; i++) {
+	for (int i = 0; i < 8; i++) {
 		m_blockList[i].setSize(Vector2f (100, 100));
 		m_blockList[i].setPosition(Vector2f(25+150*i, 200));
 	}
@@ -15,58 +15,66 @@ BlockManager::BlockManager(sfp::World* world)
 
 void BlockManager::update(sf::Event* event)
 {
-    if (event->type == sf::Event::MouseButtonPressed)
-        for (int i = 0; i < 7; i++) {
+    if (event->type == sf::Event::MouseButtonPressed) {
+        for (int i = 0; i < 8; i++) {
             if (m_blockList[i].getGlobalBounds().contains(event->mouseButton.x, event->mouseButton.y)) {
                 //m_blockList[0] correspond à BlocBackward, [1] = BlocCatch, [2] = BlocForward, [3] = BlocJump, [4] = BlocJumpForward, [5] = BlocThrow, [6] = BlocWait
                 switch (i) {
                   case 0 :
                     {
                       m_blockInstructions.push_back(new BlocBackward);
-                      m_blockInstructions.back()->setPosition(Vector2f(25 + 60 * m_blockInstructions.size(), 200));
+                      m_blockInstructions.back()->setPosition(Vector2f(25 + 60 * m_blockInstructions.size(), 500));
                       std::cout << "Print 1" << std::endl;
                       break;
                     }
                   case 1:
                   {
                       m_blockInstructions.push_back(new BlocCatch);
-                      m_blockInstructions.back()->setPosition(Vector2f(25 + 60 * m_blockInstructions.size(), 200));
+                      m_blockInstructions.back()->setPosition(Vector2f(25 + 60 * m_blockInstructions.size(), 500));
                       std::cout << "Print 2" << std::endl;
                       break;
                   }
                   case 2:
                   {
                       m_blockInstructions.push_back(new BlocForward);
-                      m_blockInstructions.back()->setPosition(Vector2f(25 + 60 * m_blockInstructions.size(), 200));
+                      m_blockInstructions.back()->setPosition(Vector2f(25 + 60 * m_blockInstructions.size(), 500));
                       std::cout << "Print 3" << std::endl;
                       break;
                   }
                   case 3:
                   {
                       m_blockInstructions.push_back(new BlocJump);
-                      m_blockInstructions.back()->setPosition(Vector2f(25 + 60 * m_blockInstructions.size(), 200));
+                      m_blockInstructions.back()->setPosition(Vector2f(25 + 60 * m_blockInstructions.size(), 500));
                       std::cout << "Print 4" << std::endl;
                       break;
                   }
                   case 4:
                   {
                       m_blockInstructions.push_back(new BlocJumpForward);
-                      m_blockInstructions.back()->setPosition(Vector2f(25 + 60 * m_blockInstructions.size(), 200));
+                      m_blockInstructions.back()->setPosition(Vector2f(25 + 60 * m_blockInstructions.size(), 500));
                       std::cout << "Print 5" << std::endl;
                       break;
                   }
                   case 5:
                   {
                       m_blockInstructions.push_back(new BlocThrow);
-                      m_blockInstructions.back()->setPosition(Vector2f(25 + 60 * m_blockInstructions.size(), 200));
+                      m_blockInstructions.back()->setPosition(Vector2f(25 + 60 * m_blockInstructions.size(), 500));
                       std::cout << "Print 6" << std::endl;
                       break;
                   }
                   case 6:
                   {
                       m_blockInstructions.push_back(new BlocWait);
-                      m_blockInstructions.back()->setPosition(Vector2f(25 + 60 * m_blockInstructions.size(), 200));
+                      m_blockInstructions.back()->setPosition(Vector2f(25 + 60 * m_blockInstructions.size(), 500));
                       std::cout << "Print 7" << std::endl;
+                      break;
+                  }
+                  case 7:
+                  {
+                      std::cout << "Delete" << std::endl;
+                      delete m_blockInstructions.back();
+                      m_blockInstructions.pop_back();
+                      std::cout << "Machin" << std::endl;
                       break;
                   }
                   default: 
@@ -75,12 +83,20 @@ void BlockManager::update(sf::Event* event)
                   }
                 }
             }
+        }    
+    }
+    if (event->key.code == sf::Keyboard::Enter) {
+        for (int i = 0; i < m_blockInstructions.size(); i++) {
+            if (m_blockInstructions.at(i)->getGlobalBounds().contains(event->mouseButton.x, event->mouseButton.y)) {
+                std::cout << "Modified" << std::endl;
+            }
         }
+    }
 }
 
 void BlockManager::draw(sf::RenderWindow* window)
 {
-    for (int i = 0; i < 7; i++) {
+    for (int i = 0; i < 8; i++) {
         window->draw(m_blockList[i]);
     }
 
--- AlgoRush/BlockManager.h
+++ AlgoRush/BlockManager.h
@@ -18,9 +18,9 @@
 class BlockManager {
 private:
 	//m_blockList[0] correspond à BlocBackward, [1] = BlocCatch, [2] = BlocForward, [3] = BlocJump, [4] = BlocJumpForward, [5] = BlocThrow, [6] = BlocWait
-	sf::RectangleShape m_blockList[7];
+	sf::RectangleShape m_blockList[8];
 	//m_blocTexture[0] correspond au textures de BlocBackward, [1] = BlocCatch, [2] = BlocForward, [3] = BlocJump, [4] = BlocJumpForward, [5] = BlocThrow, [6] = BlocWait
-	sf::Texture m_blocTexture[7];
+	sf::Texture m_blocTexture[8];
 	vector<ExecBlocs*> m_blockInstructions;
 public:
 	BlockManager(sfp::World* world);
